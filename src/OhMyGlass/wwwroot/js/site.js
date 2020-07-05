9c303ac78e6ca225fbbaf5d586c5f304c4cd5b74
Edmel Ricahuerta <exricahuerta@gmail.com>
2020-07-05 07:13:33 -0400

    init commit

---
+++ src/OhMyGlass/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿$.fn.BeerSlider = function ( options ) {
+    options = options || {};
+    return this.each(function() {
+      new BeerSlider(this, options);
+    });
+  };
+  $('.beer-slider').BeerSlider({start: 25});
