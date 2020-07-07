dc0ec9a5977abe361f84b546c000666633e61ff0
Edmel Ricahuerta <exricahuerta@gmail.com>
2020-07-07 04:33:10 -0400

    add loading

--- src/OhMyGlass/wwwroot/js/site.js
+++ src/OhMyGlass/wwwroot/js/site.js
@@ -1,7 +1,15 @@
-﻿$.fn.BeerSlider = function ( options ) {
-    options = options || {};
-    return this.each(function() {
-      new BeerSlider(this, options);
-    });
-  };
-  $('.beer-slider').BeerSlider({start: 25});
+﻿
+  $(document).ready(function() {
+    $.fn.BeerSlider = function ( options ) {
+      options = options || {};
+      return this.each(function() {
+        new BeerSlider(this, options);
+      });
+    };  
+    $('.beer-slider').BeerSlider({start: 25});
+  });
+  
+  $(window).on('load',function () {
+    $('.loading').fadeOut('slow');
+    $('.loading').remove();
+  })
