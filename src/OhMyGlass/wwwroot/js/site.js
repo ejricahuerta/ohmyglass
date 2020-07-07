cfdcce89b416c87f6a8c0d38a94842dc1ef86063
Edmel Ricahuerta <exricahuerta@gmail.com>
2020-07-07 06:16:02 -0400

    update videos

--- src/OhMyGlass/wwwroot/js/site.js
+++ src/OhMyGlass/wwwroot/js/site.js
@@ -7,6 +7,29 @@
       });
     };  
     $('.beer-slider').BeerSlider({start: 25});
+    
+    $("video").each(function(){ this.volume = 0.5; });
+
+    $('#videModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget) 
+      var src = button.data('src');
+      var modal = $(this)
+      var videosrc = modal.find('video source');
+      var video = modal.find('video');
+      $(videosrc).attr('src', src,);
+      video.get(0).load();
+      video.get(0).volume = .5;
+      video.get(0).play();
+    });
+
+    $('#videModal').on('hidden.bs.modal', function (event) {
+      var modal = $(this);
+      var videosrc = modal.find('video source');
+      var video = modal.find('video');
+      $(videosrc).attr('src', '#',);
+
+      video.get(0).pause();
+    })
   });
   
   $(window).on('load',function () {
@@ -14,4 +37,4 @@
       $('.loading').remove();
     }
     $('.loading').fadeOut(1600,removeSelf);
-  })
+  });
