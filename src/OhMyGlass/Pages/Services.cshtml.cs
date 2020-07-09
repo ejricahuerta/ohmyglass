38ec0318d36fb53a9a3f3a9630cc1991fc9adafb
Edmel Ricahuerta <exricahuerta@gmail.com>
2020-07-09 06:42:58 -0400

    add initial service page and showcase page

---
+++ src/OhMyGlass/Pages/Services.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace OhMyGlass.Pages
+{
+    public class ServicesModel : PageModel
+    {
+        private readonly ILogger<ServicesModel> _logger;
+
+        public ServicesModel(ILogger<ServicesModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet(string password = null)
+        {
+            if(!string.IsNullOrEmpty(password) && password.Equals("ohmyglass")){
+                return Page();
+            }
+            
+            if(DateTime.Now < DateTime.Parse("July 20,2020")){
+                return LocalRedirect(Url.Content("~/ComingSoon"));
+            }
+            
+            return Page();
+        }
+    }
+}
---
+++ src/OhMyGlass/Pages/Showcase.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace OhMyGlass.Pages
+{
+    public class ShowcaseModel : PageModel
+    {
+        private readonly ILogger<ShowcaseModel> _logger;
+
+        public ShowcaseModel(ILogger<ShowcaseModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet(string password = null)
+        {
+            if(!string.IsNullOrEmpty(password) && password.Equals("ohmyglass")){
+                return Page();
+            }
+            
+            if(DateTime.Now < DateTime.Parse("July 20,2020")){
+                return LocalRedirect(Url.Content("~/ComingSoon"));
+            }
+            
+            return Page();
+        }
+    }
+}
